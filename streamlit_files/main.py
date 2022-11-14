a3f3c501e532705a98972a88028bb34f92930334
gehll <gehllanza@gmail.com>
2022-11-14 20:50:03 +0100

    Initial map is ready

--- streamlit_files/main.py
+++ streamlit_files/main.py
@@ -13,17 +13,17 @@ st.text('This page shows all public transports stations in Barcelona that you ca
 and plot a marker for each public transport station, having each type of transport in a colour
 '''
 
-transports = {'Bus': {'Code': ['K014', 'K015'], 'icon': 'bus', 'tooltip': 'Bus', 'color':'red'},
-    'Bus_airport': {'Code':['K016'], 'icon': 'bus', 'tooltip': 'Bus-airpot', 'color':'red'},
-    'Bus_station': {'Code':['K017'], 'icon': 'bus', 'tooltip': 'Bus-station', 'color':'red'},
-    'Metro': {'Code':['K001'], 'icon': 'train-tunnel', 'tooltip': 'Subway', 'color':'green'},
-    'Railway': {'Code':['K002'], 'icon': 'train-track', 'tooltip': 'Railway', 'color':'green'},
-    'Renfe': {'Code':['K003'], 'icon': 'train', 'tooltip': 'RENFE', 'color':'green'},
-    'Airport_train': {'Code':['K004'], 'icon': 'train-subway', 'tooltip': 'Airport-train', 'color':'green'},
-    'Maritime_station': {'Code':['K008'], 'icon': 'ship', 'tooltip': 'Maritime-station', 'color':'blue'},
-    'Funicular': {'Code':['K009'], 'icon': 'up-from-line', 'tooltip': 'Funicular', 'color':'beige'},
-    'Cableway': {'Code':['K010'], 'icon': 'cable-car', 'tooltip': 'Cableway', 'color':'purple'},
-    'Tramvia': {'Code':['K011'], 'icon': 'train-tram', 'tooltip': 'Tramvia', 'color':'orange'}
+transports = {'Bus': {'Code': ['K014', 'K015'], 'icon': 'bus', 'tooltip': 'Bus'},
+    'Bus_airport': {'Code':['K016'], 'icon': 'bus', 'tooltip': 'Bus-airpot'},
+    'Bus_station': {'Code':['K017'], 'icon': 'bus', 'tooltip': 'Bus-station'},
+    'Metro': {'Code':['K001'], 'icon': 'subway', 'tooltip': 'Subway'},
+    'Railway': {'Code':['K002'], 'icon': 'train', 'tooltip': 'Railway'},
+    'Renfe': {'Code':['K003'], 'icon': 'train', 'tooltip': 'RENFE'},
+    'Airport_train': {'Code':['K004'], 'icon': 'train', 'tooltip': 'Airport-train'},
+    'Maritime_station': {'Code':['K008'], 'icon': 'ship', 'tooltip': 'Maritime-station'},
+    'Funicular': {'Code':['K009'], 'icon': 'arrow-up', 'tooltip': 'Funicular'},
+    'Cableway': {'Code':['K010'], 'icon': 'square-o', 'tooltip': 'Cableway'},
+    'Tramvia': {'Code':['K011'], 'icon': 'minus', 'tooltip': 'Tramvia'}
     }
 
 selected_types = st.multiselect('Choose the type of transport you want to visualize (max 2)',
@@ -38,12 +38,15 @@ barna_tooltip = "City name"
 barna_marker = Marker(barna_coords, 
                         popup="<i>Barcelona city</i>", 
                         tooltip=barna_tooltip,
-                        icon=Icon(icon="city", color='black'))
+                        icon=Icon(icon="home", color='black', prefix='fa'))
 mapa_barna = Map(location=barna_coords, zoom_start=11)
 mapa_barna.add_child(barna_marker)
 
 # Now add all public transport stations that haven been selected with the multiselect
 
+colors = ['blue', 'green']
+
+i = 0
 for key in selected_types:
     transport_type = get_data.get_transport_type(key)
     for station in transport_type:
@@ -51,6 +54,7 @@ for key in selected_types:
                 popup=f"<i>{station['Station']}</i>", 
                 tooltip=transports[key]['tooltip'],
                 icon=Icon(icon=transports[key]['icon'], 
-                        color=transports[key]['color'])).add_to(mapa_barna)
+                        color=colors[i], prefix='fa')).add_to(mapa_barna)
+    i += 1
 
 st_mapa_barna_completo = st_folium(mapa_barna)
